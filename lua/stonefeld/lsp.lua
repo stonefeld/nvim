2d29c298486162ce52d89bee9c8319c17bee0f68
stonefeld - Theo Stanfield <theo.stanfield@gmail.com>
2021-07-16 00:00:24 -0300

    Solved problem with python-language-server

--- lua/stonefeld/lsp.lua
+++ lua/stonefeld/lsp.lua
@@ -15,46 +15,44 @@ capabilities.textDocument.completion.completionItem.resolveSupport = {
 -- Run 'npm i -g bash-language-server'
 lspconfig.bashls.setup { }
 
--- Run 'npm i -g vim-language-server'.
-lspconfig.vimls.setup { }
-
--- Run 'pip3 install python-language-server[all]'.
-lspconfig.pyls.setup {
-    capabilities = capabilities,
-}
-
 -- Install 'clang' with your package manager.
 lspconfig.clangd.setup { }
-
--- Run 'npm i -g typescript typescript-language-server'.
-lspconfig.tsserver.setup { }
-
--- Run 'npm i -g vscode-html-languagueserver-bin'.
-local html_root_path = '/usr/bin/html-languageserver'
-lspconfig.html.setup {
-    capabilities = capabilities,
-    cmd = {html_root_path, '--stdio'},
-}
-
--- Run 'npm i -g vscode-css-languagueserver-bin'.
-local css_root_path = '/usr/bin/css-languageserver'
-lspconfig.cssls.setup {
-    capabilities = capabilities,
-    cmd = {css_root_path, '--stdio'},
-}
-
--- Run 'npm i -g vscode-json-languagueserver-bin'.
-local json_root_path = '/usr/bin/json-languageserver'
-lspconfig.jsonls.setup {
-    capabilities = capabilities,
-    cmd = {json_root_path, '--stdio'},
-}
 
 -- Run 'pip install cmake-language-server'.
 lspconfig.cmake.setup { }
 
--- Install 'texlab' in your system.
-lspconfig.texlab.setup { }
+-- Run 'npm i -g vscode-css-languagueserver-bin'.
+lspconfig.cssls.setup {
+    capabilities = capabilities,
+    cmd = {'css-languageserver', '--stdio'},
+}
+
+-- Run 'npm i -g vscode-html-languagueserver-bin'.
+lspconfig.html.setup {
+    capabilities = capabilities,
+    cmd = {'html-languageserver', '--stdio'},
+}
+
+-- Run 'npm i -g vscode-json-languagueserver-bin'.
+lspconfig.jsonls.setup {
+    capabilities = capabilities,
+    cmd = {'json-languageserver', '--stdio'},
+}
+
+-- Run 'pip3 install python-language-server[all]'.
+lspconfig.pylsp.setup {
+    capabilities = capabilities,
+    cmd = {'pyls'},
+}
 
 -- Install 'rust' in yout system.
 lspconfig.rls.setup { }
+
+-- Install 'texlab' in your system.
+lspconfig.texlab.setup { }
+
+-- Run 'npm i -g typescript typescript-language-server'.
+lspconfig.tsserver.setup { }
+
+-- Run 'npm i -g vim-language-server'.
+lspconfig.vimls.setup { }
--- plugin/autocmds.vim
+++ plugin/autocmds.vim
@@ -3,11 +3,13 @@ augroup AUTOCOMMANDS
   autocmd!
 
   " Set different tab width for the following filetypes.
-  autocmd FileType css,html,javascript,javascriptreact,json,jsonc,markdown,tex,typescript,typescriptreact,vim,yaml setlocal tabstop=2 shiftwidth=2
+  autocmd FileType css,html,javascript,javascriptreact,json,jsonc,markdown,tex,typescript,typescriptreact,vim,yaml setlocal tabstop=2 softtabstop=2 shiftwidth=2
   autocmd FileType html nnoremap <silent> <F5> :execute 'silent !xdg-open %'<CR>
-  autocmd FileType tex,markdown setlocal wrap nonu norelativenumber showmode laststatus=0 signcolumn=no colorcolumn=0
+  autocmd FileType tex,markdown setlocal wrap nonu norelativenumber showmode scrolloff=0 laststatus=0 signcolumn=no colorcolumn=0
   autocmd FileType tex,markdown nnoremap j gj
   autocmd FileType tex,markdown nnoremap k gk
+  autocmd FileType tex,markdown nnoremap 0 g0
+  autocmd FileType tex,markdown nnoremap $ g$
   autocmd FileType tex nnoremap <leader>st :w! \| execute '!compiler ' . expand('%:p')<CR>
   autocmd FileType markdown nnoremap <leader>st :w! \| silent execute '!compiler ' . expand('%:p') \| redraw!<CR>
   autocmd FileType tex,markdown nnoremap <silent> <leader>so :silent execute '!${PDF_READER:-zathura} ' . expand('%:r') . '.pdf &' \| redraw!<CR>
