d9a268ecaac12d9ecd2bfa19e734eb812a17edb5
stonefeld <theo.stanfield@gmail.com>
2022-03-08 09:02:28 -0300

    Added lua autocommands

--- lua/stonefeld/autocommands.lua
+++ lua/stonefeld/autocommands.lua
@@ -1,37 +1,27 @@
 -- ---------- Autocommands ---------- --
 -- Set different options and keymaps according to each filetype
-vim.cmd [[
-  aug filetype_based_changes
-    au!
-    au TermOpen term://* setl ft=term
-    au TermClose term://* exe "bd | lua Statusline('active')"
-    au BufNewFile,BufRead *.tex setl ft=tex
-    au BufNew,BufRead,BufEnter *.asm setl ft=nasm
-    au BufNew,BufRead,BufEnter *.rasi setl ft=rasi
-  aug end
-]]
+local filetype_based_changes = vim.api.nvim_create_augroup("filetype_based_changes", { clear = true })
+vim.api.nvim_create_autocmd("TermOpen", { pattern = "term://*", command = "setl ft=term", group = filetype_based_changes })
+vim.api.nvim_create_autocmd("TermClose", { pattern = "term://*", command = "bd | lua Statusline('active')", group = filetype_based_changes })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = "*.tex", command = "setl ft=tex", group = filetype_based_changes })
+vim.api.nvim_create_autocmd({ "BufNew", "BufRead", "BufEnter" }, { pattern = "*.asm", command = "setl ft=nasm", group = filetype_based_changes })
+vim.api.nvim_create_autocmd({ "BufNew", "BufRead", "BufEnter" }, { pattern = "*.rasi", command = "setl ft=rasi", group = filetype_based_changes })
 
 -- Clean and format the current buffer.
-vim.cmd [[
-  fu! FormatCode()
-    exe '%s/if(/if (/e'
-    exe '%s/for(/for (/e'
-    exe '%s/while(/while (/e'
-    exe '%s/switch(/switch (/e'
-    exe '%s/}break;/} break;/e'
-  endfu
-
-  aug clean_buffer
-    au!
-    au BufWritePre * %s/\s\+$//e
-    au FileType c,cpp au BufWritePre <buffer> call FormatCode()
-  aug end
-]]
+local clean_buffer = vim.api.nvim_create_augroup("clean_buffer", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", { buffer = 0, group = clean_buffer, callback = function()
+  vim.cmd [[ exe '%s/\s\+$//e' ]]
+end })
+vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.c", "*.cpp" }, group = clean_buffer, callback = function()
+  vim.cmd [[ exe '%s/if(/if (/e' ]]
+  vim.cmd [[ exe '%s/for(/for (/e' ]]
+  vim.cmd [[ exe '%s/while(/while (/e' ]]
+  vim.cmd [[ exe '%s/switch(/switch (/e' ]]
+  vim.cmd [[ exe '%s/}break;/} break;/e' ]]
+end })
 
 -- Get visual feedback when a line or region was yanked.
-vim.cmd[[
-  aug visual_yank
-    au!
-    au TextYankPost * silent! lua require('vim.highlight').on_yank({timeout = 100})
-  aug end
-]]
+local visual_yank = vim.api.nvim_create_augroup("visual_yank", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", { group = visual_yank, callback = function()
+  require('vim.highlight').on_yank({ timeout = 150 })
+end})
